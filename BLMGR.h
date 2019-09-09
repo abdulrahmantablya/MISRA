16dd1e3b97f05ac8e371b538e146d551e21b7b44
abdulrahmantablya <abdulrahmantablya@gmail.com>
2019-09-10 00:03:12 +0200

    BLMGR

---
+++ BLMGR.h
@@ -0,0 +1,29 @@
+﻿/*
+ * BLMGR.h
+ *
+ * Created: 28/02/2016 04:20:32 م
+ *  Author: hossam
+ */ 
+
+
+#ifndef BLMGR_H_
+#define BLMGR_H_
+
+#define SLAVE_COMM (0x00u)
+#define MSTER_COMM (0x01u)
+#define COMM_CINFIG (MSTER_COMM)
+
+#define ROLE_CHAIR (0x02u)
+#define ROLE_CAP   (0x03u)
+#define ROLE_MAPP  (0x01u)
+
+#define DEVICE_ROLE (ROLE_CHAIR)
+
+void BLMGR_Test(void);
+void BLMGR_BluetoothInit(void);
+void BLMGR_BluetoothStateMachine(void);
+void BLMGR_StartDevice(void);
+void BLMGR_SetReceiver(u8 Receiver);
+void BLMGR_SetDeviceName( u8* DeviceName,u8 DeviceNameLength);
+void BLMGR_SetBattLevel(u8 BattLevel);
+#endif /* BLMGR_H_ */
---
+++ BLMGR_CFG.h
@@ -0,0 +1,28 @@
+﻿/*
+ * BLMGR_CFG.h
+ *
+ * Created: 28/02/2016 06:54:32 م
+ *  Author: hossam
+ */ 
+
+
+#ifndef BLMGR_CFG_H_
+#define BLMGR_CFG_H_
+
+#include "Basic_Types.h"
+typedef struct  
+{
+	u8 Portname;
+	u8 Port_Mask;
+}BLMGR_DioPinConfig;
+
+#define BLOUETOOTH_ON 0xffu
+#define BUZEER_ON     0xffu
+#define BUZEER_OFF    0x00u
+
+ extern BLMGR_DioPinConfig BuzzerConfig;
+ extern BLMGR_DioPinConfig BlueToothPwrConfig;
+ extern BLMGR_DioPinConfig BluetoothKeyConfig;
+ 
+
+#endif /* BLMGR_CFG_H_ */
